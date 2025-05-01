046940e323e8038bdd4b4969c1ef453a1224e592
Esma Deniz <113371827+esmadenizerboga@users.noreply.github.com>
2025-05-01 18:31:10 +0300

    Add files via upload
    Arrangements have been made, the password has been taken under control.

--- ClassCheckk-main/classcheckk/Attendance/Attendance/RollCall/views.py
+++ ClassCheckk-main/classcheckk/Attendance/Attendance/RollCall/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.contrib.auth import logout
 
 def index(request):
     return render(request, 'index.html')  # Ana sayfa şablonunu render et
 
 def userlogin(request):
-    # Giriş işlemleri burada yapılacak (örneğin, form işleme)
-    return render(request, 'login.html')  # Giriş sayfası şablonunu render et
+    username = ''  # username değişkenini baştan tanımlıyoruz
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('empty')
+        else:
+            messages.error(request, 'Username or Password are incorrect')
+
+    return render(request, 'userlogin.html', {'username': username})
+         # Giriş sayfası şablonunu render et
+
 
 def empty(request):
     return render(request, 'empty.html')  # Empty şablonunu render et
