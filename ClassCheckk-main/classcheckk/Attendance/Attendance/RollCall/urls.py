20e9e280f65e464dbdc5cfefa7d296a3b3665377
Esma Deniz <113371827+esmadenizerboga@users.noreply.github.com>
2025-05-10 20:14:17 +0300

    Add files via upload
    Upload Files

--- ClassCheckk-main/classcheckk/Attendance/Attendance/RollCall/urls.py
+++ ClassCheckk-main/classcheckk/Attendance/Attendance/RollCall/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 # RollCall'ın urls.py si budur..
+
 urlpatterns = [
     path('', views.home, name='home'),  # Home Sayfasına Geçecek
     path('index/', views.index, name='index'),  # ClassCheck sayfası
@@ -12,5 +13,7 @@ urlpatterns = [
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('addcourse/', views.addcourse, name='addcourse'),
+    path('deletecourse/', views.deletecourse, name='deletecourse'),
 
 ]
--- ClassCheckk-main/classcheckk/Attendance/Attendance/RollCall/views.py
+++ ClassCheckk-main/classcheckk/Attendance/Attendance/RollCall/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from .models import Course
+
 
 def index(request):
     return render(request, 'index.html')  
@@ -24,8 +26,9 @@ def userlogin(request):
 
 @login_required(login_url='userlogin')
 def empty(request):
-   
-    return render(request, 'empty.html') 
+  courses = Course.objects.all()
+  return render(request, 'empty.html', {'courses': courses}) 
+
 
 @login_required(login_url='userlogin')
 def home(request):
@@ -47,7 +50,34 @@ def password_reset_confirm(request):
 def password_reset_complete(request):
      return render(request,' password_reset_complete.html')
 
+def addcourse(request):
+    if request.method == 'POST':
+        name = request.POST.get('course_name')
+        capacity = request.POST.get('course_capacity')
+        code = request.POST.get('course_code')
+        date = request.POST.get('establishment_date')
+        instructor = request.POST.get('instructor')
+
+        Course.objects.create(
+            name=name,
+            capacity=capacity,
+            code=code,
+            establishment_date=date,
+            instructor=instructor
+        )
+        return redirect('empty')  # empty adlı sayfaya yönlendir
+    return render(request, 'addcourse.html')  # add_course.html adında bir form sayfası
 
 
+def deletecourse(request):
+    courses = Course.objects.all()
+
+    if request.method == 'POST':
+        course_id = request.POST.get('course_id')
+        course = get_object_or_404(Course, id=course_id)
+        course.delete()
+        return redirect('empty')  
+
+    return render(request, 'deletecourse.html', {'courses': courses})
 
 
