7c11bd5dd7b3d23e26ed93974c291674c0caaa8e
Esma Deniz <113371827+esmadenizerboga@users.noreply.github.com>
2025-05-15 00:19:30 +0300

    Delete ClassCheckk-main/classcheckk directory

--- ClassCheckk-main/classcheckk/Attendance/RollCall/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Course
-
-class CourseForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = '__all__'  # Or list specific fields if needed
