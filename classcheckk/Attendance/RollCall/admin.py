21995ff7b4598fb182d1d9bae9ef72805ac9e3ca
Esma Deniz <113371827+esmadenizerboga@users.noreply.github.com>
2025-05-15 16:51:02 +0300

    Delete classcheckk directory

--- classcheckk/Attendance/RollCall/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Course
-from .models import Student
-
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ['course_name', 'course_capacity', 'course_code', 'establishment_date', 'instructor']
-
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Student)
